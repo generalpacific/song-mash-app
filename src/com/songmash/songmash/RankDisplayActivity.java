ce30e399344efe940231edb3a68bd689b4e4a3c0
prashantc29 <prash.c.29@gmail.com>
2014-01-27 17:09:07 -0600

    fix #7

--- src/com/songmash/songmash/RankDisplayActivity.java
+++ src/com/songmash/songmash/RankDisplayActivity.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 import android.widget.TextView;
 
 public class RankDisplayActivity extends Activity {
